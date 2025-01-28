201f4dc8c9d28590a787839b10250dfd32f1f447
Satviky <126392608+Satviky@users.noreply.github.com>
2025-01-28 07:58:48 +0000

    created handlers.rs

--- resa-backend/src/main.rs
+++ resa-backend/src/main.rs
@@ -18,28 +18,3 @@ async fn main() -> std::io::Result<()> {
     .run()
     .await
 }
-
-
-// fn main() {
-//     println!("Hello, world!");
-// }
-mod db;
-mod models;
-mod handlers;
-
-use actix_web::{web, App, HttpServer};
-
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    let client = db::connect_to_mongodb().await.unwrap();
-
-    HttpServer::new(move || {
-        App::new()
-            .app_data(web::Data::new(client.clone()))
-            .route("/messages", web::get().to(handlers::get_messages))
-            .route("/messages", web::post().to(handlers::add_message))
-    })
-    .bind("127.0.0.1:8080")?
-    .run()
-    .await
-}
