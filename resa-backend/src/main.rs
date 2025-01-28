30296b825cb1f11b0b42838ff9a4e01f9e3fded9
Satviky <126392608+Satviky@users.noreply.github.com>
2025-01-28 08:06:38 +0000

    prob-free

--- resa-backend/src/main.rs
+++ resa-backend/src/main.rs
@@ -1,6 +1,6 @@
 mod db;
 mod models;
-mod handlers;
+// mod handlers;
 
 use actix_web::{web, App, HttpServer};
 
@@ -11,8 +11,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || {
         App::new()
             .app_data(web::Data::new(client.clone()))
-            .route("/messages", web::get().to(handlers::get_messages))
-            .route("/messages", web::post().to(handlers::add_message))
+            // .route("/messages", web::get().to(handlers::get_messages))
+            // .route("/messages", web::post().to(handlers::add_message))
     })
     .bind("127.0.0.1:8080")?
     .run()
